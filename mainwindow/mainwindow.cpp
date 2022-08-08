92b427e1ac375f52802ec7fc210e3ee2d25ae27f
iwxyi <wxy19980615@gmail.com>
2022-08-08 15:14:36 +0800

    修复关闭自动更新时依旧显示“中断更新并退出”的问题

--- mainwindow/mainwindow.cpp
+++ mainwindow/mainwindow.cpp
@@ -17332,7 +17332,7 @@ void MainWindow::trayAction(QSystemTrayIcon::ActivationReason reason)
         menu->addAction(QIcon(":/icons/cry"), "退出", [=]{
             menu->close();
             prepareQuit();
-        })->text(!rt->appNewVersion.isEmpty(), "中断更新并退出")
+        })->text(ui->autoUpdateCheck->isChecked() && !rt->appNewVersion.isEmpty(), "中断更新并退出")
           ->text(isFileExist(QApplication::applicationDirPath() + "/update.zip"), "退出并更新");
         menu->exec();
     }
