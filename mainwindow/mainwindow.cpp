d3074986738c16334c32d5777518fe316f1759ce
iwxyi <wxy19980615@gmail.com>
2023-07-26 02:33:23 +0800

    代码的右键菜单的快捷键提示改为大写

--- mainwindow/mainwindow.cpp
+++ mainwindow/mainwindow.cpp
@@ -4460,7 +4460,7 @@ void MainWindow::showListMenu(QListWidget *listWidget, QString listKey, VoidFunc
     };
 
     auto menu = new FacileMenu(this);
-    menu->addAction("插入 (&a)", [=]{
+    menu->addAction("插入 (&A)", [=]{
         if (listKey == CODE_TIMER_TASK_KEY)
             addTimerTask(false, 1800, "", row);
         else if (listKey == CODE_AUTO_REPLY_KEY)
@@ -4469,18 +4469,18 @@ void MainWindow::showListMenu(QListWidget *listWidget, QString listKey, VoidFunc
             addEventAction(false, "", "", row);
         (this->*saveFunc)();
     })->disable(!item);
-    menu->addAction("上移 (&w)", [=]{
+    menu->addAction("上移 (&W)", [=]{
         moveToRow(row - 1);
     })->disable(!item || row <= 0);
-    menu->addAction("下移 (&s)", [=]{
+    menu->addAction("下移 (&S)", [=]{
         moveToRow(row + 1);
     })->disable(!item || row >= listWidget->count()-1);
-    menu->split()->addAction("复制片段 (&c)", [=]{
+    menu->split()->addAction("复制片段 (&C)", [=]{
         auto widget = listWidget->itemWidget(item);
         auto tw = static_cast<ListItemInterface*>(widget);
         QApplication::clipboard()->setText(tw->toJson().toBa());
     })->disable(!item);
-    menu->addAction("继续复制 (&n)", [=]{
+    menu->addAction("继续复制 (&N)", [=]{
         auto widget = listWidget->itemWidget(item);
         auto tw = static_cast<ListItemInterface*>(widget);
         MyJson twJson = tw->toJson();
@@ -4498,14 +4498,14 @@ void MainWindow::showListMenu(QListWidget *listWidget, QString listKey, VoidFunc
             QApplication::clipboard()->setText(QJsonDocument(array).toJson());
         }
     })->disable(!item)->hide(!canContinueCopy)->tooltip("多次复制代码片段，组合成一串json格式数组\n可用来导入导出");
-    menu->addAction("粘贴片段 (&v)", [=]{
+    menu->addAction("粘贴片段 (&V)", [=]{
         auto widget = listWidget->itemWidget(item);
         auto tw = static_cast<ListItemInterface*>(widget);
         tw->fromJson(clipJson);
 
         (this->*saveFunc)();
     })->disable(!canPaste)->tooltip("复制代码片段后可粘贴至当前位置\n可以快速导入他人的片段");
-    menu->split()->addAction("删除 (&d)", [=]{
+    menu->split()->addAction("删除 (&D)", [=]{
         auto widget = listWidget->itemWidget(item);
         auto tw = static_cast<ListItemInterface*>(widget);
 
